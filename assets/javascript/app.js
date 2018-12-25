4019669feeb26750445e454ebba56aa0d5defd4e
ttalgihon <vivithefool@gmail.com>
2018-12-25 05:41:06 -0500

    made a results section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,39 @@
 log = console.log;
 
-
 // Object of the game variables
 var results = {
     correctAnswer: 0,
-    wrongAnswer: 0,
-    countdown () {
-        let counter = 21
-        log("outer", counter);
-        setInterval(() => {
-            if (counter >= 1) {
-                counter--;
-                $(".timerplaceholder").html('Timer: '+ counter);
-                log("inner", counter);
-                clearInterval();
-            }
-        } , 500)
-        $("div#divdiv").fadeOut("slow").addClass("hiddenbody");
-        $(".displaydiv").html('The timer is finished, press done to see your results!');
-    }
+    wrongAnswer: 0,  
+};
+
+$(document).ready(function(){
+    $(".btn-warning").click(function(){
+        $(".displaydiv").hide();
+        $(".divdiv").hide();
+        $(".CAdiv").html('Correct Answer: ' + results.correctAnswer);
+        $(".WAdiv").html('Wrong Answer: ' + results.wrongAnswer);
+        log("done-button", 'banana');
+    })
+});
+
+function countdown(){
+    var counter = 21
+    log("outer", counter); //timer: 21 doesn't show
+    setInterval(() => {
+        if (counter >= 1) {
+            counter--;
+            $(".timerplaceholder").html('Timer: '+ counter);
+            log("inner", counter); //timer: 20-0 does show
+            clearInterval();
+        }
+        if (counter === 0) {
+            $("#divdiv").fadeOut("slow").addClass("hiddenbody");
+            $(".displaydiv").html('The timer is finished, press done to see your results!');
+            return;
+        }
+    } , 1000)
 }
+// The Done button shows the results of the game.
 
 // Start button press shows questions & more.
 $(document).ready(function(){
@@ -28,10 +42,28 @@ $(document).ready(function(){
         $(".btn-warning").show();
         $(".btn-success").hide();
         log("you clicked this button.") //It works.
-        results.countdown();
+        countdown();
         
     })
 }); 
+
+$(document).ready(function(){
+    $(".btn-primary").on('click', function(){
+        var buttonattrvalue = $('button').attr('id')
+        if (buttonattrvalue == 'CorA1') {
+            log('i love this thing')
+            results.correctAnswer++;
+            log(results.correctAnswer);    
+        }
+        else {
+            log('this is so wrong');
+            results.wrongAnswer++;
+            log(results.wrongAnswer);
+        }
+    })
+});
+
+
 
 //    timer = 20
 
